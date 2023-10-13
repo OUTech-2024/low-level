c44121f8ba8cf47b17fbca24b25e652648d7baf1
StarQTius <>
2023-10-13 03:32:04 +0200

    Separate HAL hacks from app

--- cmake/HAL.cmake
+++ cmake/HAL.cmake
@@ -18,7 +18,7 @@ function(add_stm32g4_driver LIB_NAME DRIVER_NAME)
         ${STM32G4_DRIVER_SOURCE_DIR}/stm32g4xx_ll_${DRIVER_NAME}.c)
     target_link_libraries(stm32g4-${LIB_NAME} PUBLIC CMSIS)
     target_include_directories(stm32g4-${LIB_NAME}
-        PUBLIC ${STM32G4_DRIVER_INCLUDE_DIR} ${PROJECT_SOURCE_DIR}/src)
+        PUBLIC ${STM32G4_DRIVER_INCLUDE_DIR} ${PROJECT_SOURCE_DIR}/src/hal)
 
     add_library(stm32g4::${LIB_NAME} ALIAS stm32g4-${LIB_NAME})
 endfunction()
