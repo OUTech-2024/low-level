a2a75f4f0b8321b4fb1773b6cbf3e551e5cf5861
StarQTius <>
2023-10-13 03:29:47 +0200

    Make timer driver available to app

--- cmake/HAL.cmake
+++ cmake/HAL.cmake
@@ -16,10 +16,9 @@ function(add_stm32g4_driver LIB_NAME DRIVER_NAME)
     add_library(stm32g4-${LIB_NAME}
         ${STM32G4_DRIVER_SOURCE_DIR}/stm32g4xx_hal_${DRIVER_NAME}.c
         ${STM32G4_DRIVER_SOURCE_DIR}/stm32g4xx_ll_${DRIVER_NAME}.c)
-    target_link_libraries(stm32g4-${LIB_NAME} PRIVATE CMSIS)
+    target_link_libraries(stm32g4-${LIB_NAME} PUBLIC CMSIS)
     target_include_directories(stm32g4-${LIB_NAME}
-        PUBLIC ${STM32G4_DRIVER_INCLUDE_DIR}
-        PRIVATE ${PROJECT_SOURCE_DIR}/src)
+        PUBLIC ${STM32G4_DRIVER_INCLUDE_DIR} ${PROJECT_SOURCE_DIR}/src)
 
     add_library(stm32g4::${LIB_NAME} ALIAS stm32g4-${LIB_NAME})
 endfunction()
--- src/stm32g4xx_hal_conf.h
+++ src/stm32g4xx_hal_conf.h
@@ -1,1 +1,4 @@
+#pragma once
+
 #include <stm32g4xx_hal_def.h>
+#include <stm32g4xx_hal_dma.h>
