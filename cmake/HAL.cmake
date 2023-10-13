30441eae479a4a262c5ff9b5f003598e9df2f160
StarQTius <>
2023-10-13 03:06:04 +0200

    Define stm32g431kb target in CMake

--- cmake/HAL.cmake
+++ cmake/HAL.cmake
@@ -6,6 +6,12 @@ set(STM32G4_DRIVER_SOURCE_DIR ${stm32cubeg4_SOURCE_DIR}/Drivers/STM32G4xx_HAL_Dr
 set(STM32G4_DRIVER_INCLUDE_DIR ${stm32cubeg4_SOURCE_DIR}/Drivers/STM32G4xx_HAL_Driver/Inc)
 set(STM32G431KB_LINKER_SCRIPT ${stm32cubeg4_SOURCE_DIR}/Projects/NUCLEO-G431KB/Templates/STM32CubeIDE/STM32G431KBTX_FLASH.ld)
 
+function(add_stm32_target LIB_NAME COMPILE_DEF LINKER_SCRIPT)
+    add_library(${LIB_NAME} INTERFACE)
+    add_compile_definitions(${LIB_NAME} PUBLIC ${COMPILE_DEF})
+    set_target_properties(${LIB_NAME} PROPERTIES LINK_DEPENDS ${LINKER_SCRIPT})
+endfunction()
+
 function(add_stm32g4_driver LIB_NAME DRIVER_NAME)
     add_library(stm32g4-${LIB_NAME}
         ${STM32G4_DRIVER_SOURCE_DIR}/stm32g4xx_hal_${DRIVER_NAME}.c
@@ -17,6 +23,8 @@ function(add_stm32g4_driver LIB_NAME DRIVER_NAME)
 
     add_library(stm32g4::${LIB_NAME} ALIAS stm32g4-${LIB_NAME})
 endfunction()
+
+add_stm32_target(stm32g431kb STM32G431xx ${STM32G431KB_LINKER_SCRIPT})
 
 add_stm32g4_driver(timer tim)
 
