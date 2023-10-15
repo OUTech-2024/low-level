32b5d4339d3171ddf22706f4bbefacf88add453c
StarQTius <>
2023-10-16 00:26:33 +0200

    Update main.cpp for IWYU

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,8 +1,8 @@
-#include <stm32g4xx_hal.h>
+#include <stm32g4xx_hal.h> // IWYU pragma: keep
 #include <stm32g4xx_hal_tim.h>
 
 int main() {
-  HAL_TIM_Encoder_Init();
+  HAL_TIM_Encoder_Init(nullptr, nullptr);
 
   return 0;
 }
